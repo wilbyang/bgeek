d2649f152146405c8f60cf79fdd18773b5f7f503
wilbyang <yang.wilby@gmail.com>
2023-09-19 11:40:49 +0200

    use the pipe

--- src/app/linkify.pipe.ts
+++ src/app/linkify.pipe.ts
@@ -1,12 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
+const urlRegex = /(https?:\/\/[^\s]+)/g;
 @Pipe({
   name: 'linkify'
 })
 export class LinkifyPipe implements PipeTransform {
 
   transform(value: string, ...args: unknown[]): string {
-    const urlRegex = /(https?:\/\/[^\s]+)/g;
     return value.replace(urlRegex, '<a href="$1">$1</a>');
   }
 
