a6a9b9072280187166e5de850e370e3e15a47f17
wilbyang <yang.wilby@gmail.com>
2023-10-27 10:35:46 +0200

    let tags have a seperate page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,9 +7,11 @@ import { ColumnComponent } from './column/column.component';
 import {TweetComponent} from "./tweet/tweet.component";
 import {JuejieColumnComponent} from "./juejie-column/juejie-column.component";
 import {JuejieArticleComponent} from "./juejie-article/juejie-article.component";
+import {TagsComponent} from "./tags/tags.component";
 const routes: Routes = [
   { path: '', redirectTo: '/tweets', pathMatch: 'full' },
   { path: 'columns', component: ColumnComponent },
+  { path: 'columns/tags', component: TagsComponent },
   { path: 'columns/:cid/articles/:aid', component: ArticleComponent },
   { path: 'juejin', component: JuejieColumnComponent },
   { path: 'juejin/article', component: JuejieArticleComponent },
--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {GeekService} from "../geek.service";
+import {Article} from "../models";
 
 @Component({
   selector: 'app-tags',
@@ -9,8 +10,18 @@ import {GeekService} from "../geek.service";
 })
 export class TagsComponent {
   constructor(private http: HttpClient, private geekbangService: GeekService) {
+
+    this.geekbangService.$articles.subscribe({
+      next: result => {
+        this.articles = result;
+
+
+      }, error: error => console.error(error)
+    });
   }
   tags: string[] = [];
+  articles: Article[] | undefined;
+
   ngOnInit() {
     this.http.get<string[]>('http://localhost:8800/geekbang/tags').subscribe((data: any) => {
       this.tags = data.map((v: string) => v.split(':')[1]);
